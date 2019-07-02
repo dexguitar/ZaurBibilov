8629dc5cc523627c8c796a19c0f390c7d7cbf25d
dexguitar <stinzane@gmail.com>
2019-07-02 10:05:25 +0300

    misc45

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -75,8 +75,8 @@ public class BaseTest {
         WebElement passField = driver.findElement(By.id("password"));
         passField.sendKeys(password);
         WebElement loginButton = driver.findElement(By.id("login-button"));
-//        actions.moveToElement(loginButton).click().build().perform();
-        loginButton.click();
+        actions.moveToElement(loginButton, 110, 221).click().build().perform();
+//        loginButton.click();
 
 
 //        Assert User name in the left-top side of screen that user is logged in
